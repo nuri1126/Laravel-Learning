db462761e6bb84db5378982fbe16750c2659482a
Nuli Bang <nulibang@Nulis-MacBook-Pro.local>
2019-12-11 13:49:45 -0500

    Controllers 101

---
+++ MyFirstSite/app/Http/Controllers/PagesController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PagesController extends Controller
+{
+   public function home(){
+   		return view('welcome')->with([
+   			'foo' => 'bar',
+   			'tasks' => [
+   				'Go to the store',
+   				'Go to the market',
+   				'Go to work'
+   			]
+   		]);
+   }
+
+   public function about(){
+   		return view('about');
+   }
+
+   public function contact(){
+   		return view('contact');
+   }
+}
--- MyFirstSite/routes/web.php
+++ MyFirstSite/routes/web.php
@@ -1,49 +1,11 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+//catch all controler, page controller --> migrate page
 
-Route::get('/', function () {
-	// $tasks = [
-	// 	'Go to the store',
-	// 	'Go to the market',
-	// 	'Go to work'
-	// ];
- //    return view('welcome',[
- //    	'tasks' => $tasks,
- //    	'foo' => request('title')
- //    ]);
-	// return  view('welcome')->withTasks($tasks);
-	// return view('welcome')->withTasks([
-	// 	'Go to the store',
-	// 	'Go to the market',
-	// 	'Go to work'
-	// ]);
-	return view('welcome')->with([
-		'foo' => 'bar',
-		'tasks' => [
-			'Go to the store',
-			'Go to the market',
-			'Go to work'
-		]
-	]);
-});
+Route::get('/', 'PagesController@home');
+Route::get('/about', 'PagesController@about');
+Route::get('/contact', 'PagesController@contact');
 
- Route::get('/about', function () {
-    return view('about');
-});
-
-Route::get('/contact', function () {
-    return view('contact');
-});
-
+//PrpjetsController, TasksController, UsersController
 
 
