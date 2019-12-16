c7db29419743298f3052ad9a9bed497ac6ee48c9
Nuli Bang <nulibang@Nulis-MacBook-Pro.local>
2019-12-16 12:04:37 -0500

    Databases and Migrations

---
+++ project/database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    //forward operation, when you run "php artisan migrate"
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('username');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    //reverse operation, when you run "php artisan migrate:rollback"
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+
+    //"php artisan migrate:fresh" >> just drop all the table and re-migrate all of them
+}
